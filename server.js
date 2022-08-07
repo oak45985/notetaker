e67516835b3783e16d2c9bd13a1aebafc650c3ef
oak45985 <oak45985@gmail.com>
2022-08-07 18:28:29 -0500

    updated routing for serverjs hmtlroutes/indexjs

---
+++ server.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const PORT = process.eventNames.PORT || 3002;
+const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const hmtlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+const { notes } = require('./develop/db/db.json');
+
+app.use('/api', apiRoutes);
+
+app.use('/', hmtlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`)
+});
