db597666e5d40505e5d6a42af97f986fb3b9a280
oak45985 <oak45985@gmail.com>
2022-08-10 17:51:41 -0500

    updated basic routing

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const { /* formulas for filtering */ } = require('../../develop/public/assets/js/index');
+const { notes } = require('../../develop/db/db.json');
+
+
+
+module.exports = router;
